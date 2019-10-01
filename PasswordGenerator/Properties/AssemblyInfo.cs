045b7a6fb33c4783ccecdf7462f84de946cb4d29
Mark Khromov <lane.71rus@gmail.com>
2019-10-01 21:26:12 +0300

    Added base project files.

---
+++ PasswordGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Password Generator")]
+[assembly: AssemblyProduct("Password Generator")]
+[assembly: AssemblyCopyright("Copyright © Mark Khromov 2019")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ea46f868-f795-4958-b379-aa2470028631")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
