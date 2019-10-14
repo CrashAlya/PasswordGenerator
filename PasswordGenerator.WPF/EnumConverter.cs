81dd9d1c20d91468a1cc65559a341ef3869c26ff
Alya <krashalya@icloud.com>
2019-10-15 01:35:44 +0300

    Added enum converter.

---
+++ PasswordGenerator.WPF/EnumConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace PasswordGenerator.WPF {
+    public class EnumConverter : MarkupExtension, IValueConverter {
+        public Type Type { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(Type == null) {
+                throw new ArgumentNullException(nameof(Type));
+            }
+            if(value == null) {
+                return null;
+            }
+            if(value.GetType() != Type) {
+                throw new ArgumentException();
+            }
+            return value.ToString();
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(Type == null) {
+                throw new ArgumentNullException(nameof(Type));
+            }
+            if(value == null) {
+                return null;
+            }
+            return Enum.Parse(Type, value.ToString());
+        }
+    }
+}
