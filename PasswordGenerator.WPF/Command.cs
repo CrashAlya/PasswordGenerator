9107b9ada704374dd98f097b388b20ba7994b7f5
Alya <krashalya@icloud.com>
2019-10-15 00:59:29 +0300

    Added class for command.

---
+++ PasswordGenerator.WPF/Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PasswordGenerator.WPF {
+    public class Command : ICommand {
+        public Command(Action action) : this(_ => action()) { }
+        public Command(Action<object> action) {
+            this.action = action;
+        }
+
+        readonly Action<object> action;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+
+        public void Execute(object parameter) {
+            action?.Invoke(parameter);
+        }
+    }
+}
