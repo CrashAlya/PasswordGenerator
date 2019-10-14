0ebd855c6c390d577dd9b42fb9dffda4c89d6c51
Alya <krashalya@icloud.com>
2019-10-15 01:11:37 +0300

    Added enum to items source converter.

---
+++ PasswordGenerator.WPF/EnumItemsSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace PasswordGenerator.WPF {
+    public class EnumItemsSource : MarkupExtension {
+        public EnumItemsSource(Type type) {
+            this.type = type;
+        }
+
+        readonly Type type;
+
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return Enum.GetValues(type);
+        }
+    }
+}
